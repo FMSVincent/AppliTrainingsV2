1f8e2d431d0fcc02582ea113828cba545069a99a
vincent <vincent.bickel@fms-ea.com>
2024-11-22 15:31:15 +0100

    fix redirect customer if not connected

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -3,35 +3,49 @@ import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
 import { Customer } from 'src/app/model/customer.model';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AuthenticateService } from 'src/app/services/authenticate.service';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
+  myForm: FormGroup;
 
-  myForm : FormGroup;
-
-  constructor(public cartService: CartService, private router: Router) {
+  constructor(
+    public cartService: CartService,
+    private router: Router,
+    private authService: AuthenticateService
+  ) {
     let customer = this.cartService.getCustomer();
     this.myForm = new FormGroup({
-      firstname : new FormControl(customer.firstname),
-      lastname : new FormControl(customer.lastname),
-      adress : new FormControl(customer.adress),
-      mobile : new FormControl(customer.mobileNumber),
-      mail : new FormControl(customer.mail)
-    })
-
-   }
+      firstname: new FormControl(customer.firstname),
+      lastname: new FormControl(customer.lastname),
+      adress: new FormControl(customer.adress),
+      mobile: new FormControl(customer.mobileNumber),
+      mail: new FormControl(customer.mail),
+    });
+  }
 
   ngOnInit(): void {
-   }
+    !this.authService.isLogin()
+      ? this.router.navigateByUrl('/trainings')
+      : false;
+  }
 
-  onSaveCustomer(form : FormGroup) {
-    if(form.valid){
-      this.cartService.saveCustomer(new Customer(form.value.firstname,form.value.lastname,form.value.adress,form.value.mobile,form.value.mail));
-      this.router.navigateByUrl('/order');        
+  onSaveCustomer(form: FormGroup) {
+    if (form.valid) {
+      this.cartService.saveCustomer(
+        new Customer(
+          form.value.firstname,
+          form.value.lastname,
+          form.value.adress,
+          form.value.mobile,
+          form.value.mail
+        )
+      );
+      this.router.navigateByUrl('/order');
     }
   }
 }
